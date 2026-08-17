0a7d0bec6a6188d58e80fb5fb871645b08794860
Julio Figueiredo <julioo.1amorim@gmail.com>
2026-08-16 21:13:09 -0300

    🔧 chore(alembic): fix metadata imports and track versions directory

--- alembic/env.py
+++ alembic/env.py
@@ -5,9 +5,10 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+import app.models.user  # noqa: F401
 from alembic import context
 from app.core.config import settings
-from app.models.user import Base
+from app.db.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
